1804e4842cb8f5deabb87723a5c3d481321bfff2
Claudia Pando <claudiamfp@hotmail.com>
2024-10-02 15:18:43 +0100

    Solved lab

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}.`
 
 // Print out the concatenated string
+
+console.log(tongueTwister)
 
 
 
@@ -22,10 +24,15 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const part1LastLetter = part1.slice(0,-1);
+const part1UpperCase = part1LastLetter + part1.slice(-1).toUpperCase();
+
+const part2LastLetter = part2.slice(0,-1);
+const part2UpperCase = part2LastLetter + part2.slice(-1).toUpperCase();
 
 
 // Print the cameLtaiL-formatted string
-
+console.log(part1UpperCase + part2UpperCase )
 
 
 
@@ -36,9 +43,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = billTotal*15/100;
+
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,11 +56,12 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.random()*10
+const randomNumberInteger = Math.round(randomNumber)
 
 // Print the generated random number
 
-
+console.log(randomNumberInteger)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +72,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+console.log(expression1)
 
 const expression2 = a || b;
+console.log(expression2)
 
 const expression3 = !a && b;
+console.log(expression3)
 
 const expression4 = !(a && b);
+console.log(expression4)
 
 const expression5 = !a || !b;
+console.log(expression5)
 
 const expression6 = !(a || b);
+console.log(expression6)
 
 const expression7 = a && a;
+console.log(expression7)
